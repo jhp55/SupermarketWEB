78dfc63cd0bca6ae35e5fe4d332d625f18680008
JAUN DAVID HERNANDEZ PERDOMO <juandavidhernandez515@hotmail.com>
2024-10-29 23:16:10 -0500

    Modelos y configuración del DbContext

---
+++ Data/SumpermarketContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SupermarketWEB.Models;
+
+namespace SupermarketWEB.Data
+{
+    public class SumpermarketContext : DbContext
+    {
+        public SumpermarketContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Pay_Mode> PayModes { get; set; }
+        public DbSet<Customers> Customers { get; set; }
+    }
+}
---
+++ Models/Customers.cs
@@ -0,0 +1,14 @@
+﻿namespace SupermarketWEB.Models
+{
+    public class Customers
+    {
+        public int Id { get; set; }
+        public int Document_Number { get; set; }
+        public string First_Name { get; set; }
+        public string Last_Name { get; set; }
+        public string Address { get; set; }
+        public string Date { get; set; }
+        public string Phone_Number { get; set; }
+        public string Email { get; set; }
+    }
+}
