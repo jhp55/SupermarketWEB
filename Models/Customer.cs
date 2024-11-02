a88f0506a33e7a59ce9ccf4350e5b460723fb4ed
JAUN DAVID HERNANDEZ PERDOMO <juandavidhernandez515@hotmail.com>
2024-11-02 01:03:08 -0500

    Create  Categories

--- Models/Customer.cs
+++ Models/Customer.cs
@@ -7,7 +7,7 @@
         public string First_Name { get; set; }
         public string Last_Name { get; set; }
         public string Address { get; set; }
-        public string Date { get; set; }
+        public string birthday { get; set; }
         public string Phone_Number { get; set; }
         public string Email { get; set; }
     }
