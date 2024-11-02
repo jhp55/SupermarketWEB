f6f1a3149f7c367d9867d6e4969e5fb3adbf797f
JAUN DAVID HERNANDEZ PERDOMO <juandavidhernandez515@hotmail.com>
2024-11-02 11:08:50 -0500

    Create Pay Mode

---
+++ Pages/Pay_Mode/Create.cshtml.cs
@@ -0,0 +1,38 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using SupermarketWEB.Data;
+using SupermarketWEB.Models;
+
+namespace SupermarketWEB.Pages.Pay_Mode
+{
+    public class CreateModel : PageModel
+    {
+		private readonly SumpermarketContext _context;
+
+		public CreateModel(SumpermarketContext context)
+		{
+			_context = context;
+		}
+
+		public IActionResult OnGet()
+		{
+			return Page();
+		}
+
+		[BindProperty]
+		public PayMode PayMode { get; set; } = default!;
+
+		public async Task<IActionResult> OnPostAsync()
+		{
+			if (!ModelState.IsValid || _context.PayModes == null || PayMode == null)
+			{
+				return Page();
+			}
+
+			_context.PayModes.Add(PayMode);
+			await _context.SaveChangesAsync();
+
+			return RedirectToPage("./Index");
+		}
+	}
+}
