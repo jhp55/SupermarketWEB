f77705e0a98f0a3abab032e3f9d27970b232058d
JAUN DAVID HERNANDEZ PERDOMO <juandavidhernandez515@hotmail.com>
2024-11-02 10:25:42 -0500

    Create Products

--- Models/Product.cs
+++ Models/Product.cs
@@ -11,6 +11,6 @@ namespace SupermarketWEB.Models
         public decimal Price { get; set; }
         public int Stock { get; set; }
         public int CategoryId { get; set; }
-        public Category Category { get; set; }
+        public Category? Category { get; set; }
     }
 }
