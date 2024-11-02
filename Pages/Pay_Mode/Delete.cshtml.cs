bea311fca35783209b51a0644f4da79ec72fb17b
JAUN DAVID HERNANDEZ PERDOMO <juandavidhernandez515@hotmail.com>
2024-11-02 11:30:01 -0500

    Delete Pay Mode

---
+++ Pages/Pay_Mode/Delete.cshtml.cs
@@ -0,0 +1,58 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using SupermarketWEB.Data;
+using SupermarketWEB.Models;
+
+namespace SupermarketWEB.Pages.Pay_Mode
+{
+    public class DeleteModel : PageModel
+    {
+		private readonly SumpermarketContext _context;
+
+		public DeleteModel(SumpermarketContext context)
+		{
+			_context = context;
+		}
+
+		[BindProperty]
+		public PayMode PayMode { get; set; } = default!;
+
+		public async Task<IActionResult> OnGetAsync(int? id)
+		{
+			if (id == null || _context.PayModes == null)
+			{
+				return NotFound();
+			}
+			var payMode = await _context.PayModes.FirstOrDefaultAsync(m => m.Id == id);
+
+			if (payMode == null)
+			{
+				return NotFound();
+			}
+			else
+			{
+				PayMode = payMode;
+			}
+			return Page();
+		}
+
+		public async Task<IActionResult> OnPostAsync(int? id)
+		{
+			if (id == null || _context.PayModes == null)
+			{
+				return NotFound();
+			}
+			var payMode = await _context.PayModes.FindAsync(id);
+
+			if (payMode != null)
+			{
+				payMode = payMode;
+				_context.PayModes.Remove(payMode);
+				await _context.SaveChangesAsync();
+			}
+
+			return RedirectToPage("./Index");
+		}
+	}
+}
